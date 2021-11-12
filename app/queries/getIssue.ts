2c7a8da62bbf80658ff00ff1ae3eb99cc354b111
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-12 15:11:58 +0100

    query user email only

--- app/queries/getIssue.ts
+++ app/queries/getIssue.ts
@@ -2,7 +2,16 @@ import { NotFoundError } from "blitz"
 import db from "db"
 
 export default async function getIssue(id: number) {
-  const issue = await db.issue.findUnique({ where: { id: id }, include: { createdBy: true } })
+  const issue = await db.issue.findUnique({
+    where: { id: id },
+    include: {
+      createdBy: {
+        select: {
+          email: true,
+        },
+      },
+    },
+  })
   console.log("###", issue)
   if (!issue) {
     throw new NotFoundError()
