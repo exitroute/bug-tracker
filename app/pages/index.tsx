0307df6dfa4eaaf9e81e4071c8c81e489f06c624
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-15 21:07:57 +0100

    remove logged in user data and set redirect for logged in users to home

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,5 +1,6 @@
+import { useEffect } from "react"
 import { Suspense } from "react"
-import { Image, Link, BlitzPage, useMutation, Routes, useQuery } from "blitz"
+import { Image, Link, BlitzPage, useMutation, Routes, getSession } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 import logout from "app/auth/mutations/logout"
@@ -9,61 +10,27 @@ import logout from "app/auth/mutations/logout"
  * You can delete everything in here and start from scratch if you like.
  */
 
-const UserInfo = () => {
-  const currentUser = useCurrentUser()
-  const [logoutMutation] = useMutation(logout)
-
-  if (currentUser) {
-    return (
-      <>
-        <button
-          className="button small"
-          onClick={async () => {
-            await logoutMutation()
-          }}
-        >
-          Logout
-        </button>
-        <div>
-          User email: <code>{currentUser.email}</code>
-          <br />
-          User id: <code>{currentUser.id}</code>
-          <br />
-          User role: <code>{currentUser.role}</code>
-        </div>
-      </>
-    )
-  } else {
-    return (
-      <>
-        <Link href={Routes.SignupPage()}>
-          <a className="button small">
-            <strong>Sign Up</strong>
-          </a>
-        </Link>
-        <Link href={Routes.LoginPage()}>
-          <a className="button small">
-            <strong>Login</strong>
-          </a>
-        </Link>
-      </>
-    )
-  }
-}
-
 const LandingPage: BlitzPage = () => {
   return (
     <div className="container">
       <h1>Bug Tracker</h1>
       <p>An ExitRoute Project by Ryan O&apos;Shea</p>
-      <Suspense fallback="Loading...">
-        <UserInfo />
-      </Suspense>
+      <Link href={Routes.SignupPage()}>
+        <a className="button small">
+          <strong>Sign Up</strong>
+        </a>
+      </Link>
+      <br />
+      <Link href={Routes.LoginPage()}>
+        <a className="button small">
+          <strong>Login</strong>
+        </a>
+      </Link>
     </div>
   )
 }
 
 LandingPage.suppressFirstRenderFlicker = true
-LandingPage.getLayout = (page) => <Layout title="Landing Page">{page}</Layout>
+LandingPage.redirectAuthenticatedTo = "/home"
 
 export default LandingPage
