5c981736499925843a5cb88dc6d33616470cd08a
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-15 19:08:05 +0100

    instantiate create profile page

---
+++ app/users/createProfile.tsx
@@ -0,0 +1,11 @@
+/**
+ * This is where the user arrives after signing up
+ */
+
+import { BlitzPage } from "next"
+
+const CreateProfile: BlitzPage = () => {
+  return <h1>Create Profile</h1>
+}
+
+export default CreateProfile
