ac74c887b388334ad4b06b892520efe289dc888e
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-15 19:05:57 +0100

    add layout styles to layout component

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,12 +1,14 @@
 import { Head, BlitzLayout } from "blitz"
+import styles from "./Layout.module.css"
 
 const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Head>
         <title>{title || "bug-tracker"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className={styles.content}>{children}</div>
 
       {children}
     </>
