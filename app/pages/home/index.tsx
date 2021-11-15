3921afb9cf40093c91b8f3d5cde0fc29bfec0d60
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-15 16:06:17 +0100

    create home page

--- app/pages/home/index.tsx
+++ app/pages/home/index.tsx
@@ -1,3 +1,10 @@
+import { Suspense } from "react"
+import { Image, Link, BlitzPage, useMutation, Routes, useQuery } from "blitz"
+import Layout from "app/core/layouts/Layout"
+import getIssues from "app/queries/getIssues"
+import logout from "app/auth/mutations/logout"
+import { useCurrentUser } from "app/core/hooks/useCurrentUser"
+
 /**
  * this is where the user arrives after logging in
  *
@@ -16,13 +23,63 @@
  *
  */
 
-import { Image, Link, BlitzPage, useMutation, Routes, useQuery } from "blitz"
-import Layout from "app/core/layouts/Layout"
-
-const Home: BlitzPage = () => {
-  return <h1 data-testid="page-home">Home page</h1>
+const IssueList = () => {
+  const [issues] = useQuery(getIssues, undefined)
+  return (
+    <ul>
+      {issues.map((issue) => (
+        <Link key={issue.id} href={Routes.IssuePage({ issueId: issue.id })}>
+          <a>
+            <li>{issue.title}</li>
+          </a>
+        </Link>
+      ))}
+    </ul>
+  )
 }
 
+const UserInfo = () => {
+  const currentUser = useCurrentUser()
+
+  if (currentUser) {
+    return (
+      <div>
+        User email: <code>{currentUser.email}</code>
+        <br />
+        User id: <code>{currentUser.id}</code>
+        <br />
+        User role: <code>{currentUser.role}</code>
+      </div>
+    )
+  } else {
+    return null
+  }
+}
+
+const Home: BlitzPage = () => {
+  const [logoutMutation] = useMutation(logout)
+  return (
+    <main>
+      <h1>Home page</h1>
+      <Suspense fallback="Loading...">
+        <UserInfo />
+      </Suspense>
+      <button
+        className="button small"
+        onClick={async () => {
+          await logoutMutation()
+        }}
+      >
+        Logout
+      </button>
+      <Suspense fallback="loading">
+        <IssueList />
+      </Suspense>
+    </main>
+  )
+}
+
+Home.authenticate = true
 Home.suppressFirstRenderFlicker = true
 Home.getLayout = (page) => <Layout title="Home">{page}</Layout>
 
