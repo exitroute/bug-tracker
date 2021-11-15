180814a33bb37b983cd117ac23cb2a42230d30fe
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-15 20:01:02 +0100

    remove button and user info

--- app/pages/issues/index.tsx
+++ app/pages/issues/index.tsx
@@ -4,11 +4,9 @@
  * */
 
 import { Suspense } from "react"
-import { Link, BlitzPage, useMutation, Routes, useQuery } from "blitz"
+import { Link, BlitzPage, Routes, useQuery } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import getIssues from "app/queries/getIssues"
-import logout from "app/auth/mutations/logout"
-import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 
 const IssueList = () => {
   const [issues] = useQuery(getIssues, undefined)
@@ -25,40 +23,9 @@ const IssueList = () => {
   )
 }
 
-const UserInfo = () => {
-  const currentUser = useCurrentUser()
-
-  if (currentUser) {
-    return (
-      <div>
-        User email: <code>{currentUser.email}</code>
-        <br />
-        User id: <code>{currentUser.id}</code>
-        <br />
-        User role: <code>{currentUser.role}</code>
-      </div>
-    )
-  } else {
-    return null
-  }
-}
-
 const Issues: BlitzPage = () => {
-  const [logoutMutation] = useMutation(logout)
   return (
     <main>
-      <h1>Issue Page</h1>
-      <Suspense fallback="Loading...">
-        <UserInfo />
-      </Suspense>
-      <button
-        className="button small"
-        onClick={async () => {
-          await logoutMutation()
-        }}
-      >
-        Logout
-      </button>
       <Suspense fallback="loading">
         <IssueList />
       </Suspense>
