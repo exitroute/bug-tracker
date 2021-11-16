96fca410f5df10ce7fc2e1dbf7566e764e22ab74
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-16 14:21:49 +0100

    add user profile files

--- app/pages/profiles/[userId].tsx
+++ app/pages/profiles/[userId].tsx
@@ -1,7 +1,11 @@
+/**
+ * This is where a user sees a specific user profile
+ */
+
 import { BlitzPage } from "next"
 
 const UserProfile: BlitzPage = () => {
-  return <h1>Create Profile</h1>
+  return <h1>User Profile</h1>
 }
 
 export default UserProfile
---
+++ app/pages/profiles/index.tsx
@@ -0,0 +1,11 @@
+/**
+ * This is where a user sees all the user profiles
+ */
+
+import { BlitzPage } from "next"
+
+const ProfileList: BlitzPage = () => {
+  return <h1>Profile List</h1>
+}
+
+export default ProfileList
