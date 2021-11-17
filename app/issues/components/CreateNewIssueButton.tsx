570ceefb5ceeb9f36c815892ce1c8bd171446867
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-17 16:21:15 +0100

    instantiate create new issue button

---
+++ app/issues/components/CreateNewIssueButton.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+// takes user to the create new issue page
+
+// add this to every list page
+
+const CreateNewIssueButton = () => {
+  return <button>Create Issue</button>
+}
+
+export default CreateNewIssueButton
