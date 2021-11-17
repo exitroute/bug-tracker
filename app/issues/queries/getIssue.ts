e0b7394fb78706aeb14f2c96c0875a28ca4eba6c
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-17 15:00:51 +0100

    move to issue/queries

---
+++ app/issues/queries/getIssue.ts
@@ -0,0 +1,19 @@
+import { NotFoundError } from "blitz"
+import db from "db"
+
+export default async function getIssue(id: number) {
+  const issue = await db.issue.findUnique({
+    where: { id: id },
+    include: {
+      createdBy: {
+        select: {
+          email: true,
+        },
+      },
+    },
+  })
+  if (!issue) {
+    throw new NotFoundError()
+  }
+  return issue
+}
