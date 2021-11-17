88d67bd4f6f33020b2c2632f70e0db7b96238d4d
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-17 16:24:11 +0100

    add page authorisation and redirect function

--- app/pages/issues/new.tsx
+++ app/pages/issues/new.tsx
@@ -1,60 +1,25 @@
 /**
  * This is where the user makes a new issue
+ *
+ * [ ] Add redirect to [issue] on success
+ *
  */
-import { Form, Field } from "react-final-form"
-import { BlitzPage, useMutation } from "blitz"
-import createIssue from "app/issues/mutations/createIssue"
-import { string } from "zod"
+import { useRouter, BlitzPage, Routes } from "blitz"
+import NewIssueForm from "app/issues/components/NewIssueForm"
 
 const NewIssuePage: BlitzPage = () => {
-  const [createIssueMutation] = useMutation(createIssue)
+  const router = useRouter()
 
   return (
     <>
       <h1>Create A New Issue</h1>
       <div>
-        <Form
-          onSubmit={async (values) => {
-            try {
-              const issue = await createIssueMutation(values)
-            } catch (error) {
-              console.log(error)
-            }
-          }}
-          initialValues={{
-            title: "",
-            description: "",
-            createdBy: {},
-          }}
-          render={({ handleSubmit, form, submitting, pristine, values }) => (
-            <form onSubmit={handleSubmit}>
-              <div>
-                <label>Title</label>
-                <br />
-                <Field
-                  name="title"
-                  component="input"
-                  type="text"
-                  placeholder="Add a descriptive title"
-                />
-              </div>
-              <div>
-                <label>Expected behavior</label>
-                <br />
-                <Field name="description" component="textarea" placeholder="What happened?" />
-              </div>
-              <button type="submit" disabled={submitting || pristine}>
-                Submit
-              </button>
-              <button type="button" onClick={form.reset} disabled={submitting || pristine}>
-                Reset
-              </button>
-            </form>
-          )}
-        />
+        <NewIssueForm onSuccess={(issue) => router.push(Routes.IssuePage({ issueId: issue.id }))} />
       </div>
     </>
   )
 }
 
+NewIssuePage.authenticate = true
+NewIssuePage.suppressFirstRenderFlicker = true
 export default NewIssuePage
