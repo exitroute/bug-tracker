bb2147d57c3187c566962e48ad67a2b5323d81f8
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-17 15:03:44 +0100

    add mutation to create issue

---
+++ app/issues/mutations/createIssue.ts
@@ -0,0 +1,20 @@
+import { Ctx } from "blitz"
+import db from "db"
+
+export default async function createIssue(input: any, ctx: Ctx) {
+  ctx.session.$authorize()
+
+  const issue = await db.issue.create({
+    data: {
+      title: input.title,
+      description: input.description,
+      createdBy: {
+        connect: {
+          id: ctx.session.userId,
+        },
+      },
+    },
+  })
+
+  return issue
+}
