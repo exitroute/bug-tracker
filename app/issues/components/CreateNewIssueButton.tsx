4460005de9d91acf6aa6b36a5613de0404e01cbf
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-18 11:25:54 +0100

    add styles and routes to button

--- app/issues/components/CreateNewIssueButton.tsx
+++ app/issues/components/CreateNewIssueButton.tsx
@@ -1,11 +1,19 @@
+import { Link, Routes } from "blitz"
 import React from "react"
+import styles from "./CreateNewIssueButton.module.css"
 
 // takes user to the create new issue page
 
 // add this to every list page
 
 const CreateNewIssueButton = () => {
-  return <button>Create Issue</button>
+  return (
+    <div className={styles.wrapper}>
+      <Link href={Routes.NewIssuePage()}>
+        <a className={styles.anchor}>Create Issue</a>
+      </Link>
+    </div>
+  )
 }
 
 export default CreateNewIssueButton
