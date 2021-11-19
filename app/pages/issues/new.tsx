9291b07379ee39f90882bbf4cc7588bd1b3d33cd
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-19 01:28:45 +0100

    move issueForm to new means different mutations can be passed to issueForm

--- app/pages/issues/new.tsx
+++ app/pages/issues/new.tsx
@@ -4,17 +4,32 @@
  * [ ] Add redirect to [issue] on success
  *
  */
-import { useRouter, BlitzPage, Routes } from "blitz"
-import NewIssueForm from "app/issues/components/NewIssueForm"
+import { useRouter, BlitzPage, Routes, useMutation } from "blitz"
+import { IssueForm } from "app/issues/components/IssueForm"
+import createIssue from "app/issues/mutations/createIssue"
 
 const NewIssuePage: BlitzPage = () => {
   const router = useRouter()
+  const [createIssueMutation] = useMutation(createIssue)
+  const initialValues = { title: "", description: "" }
+  const onSuccess = (issue) => router.push(Routes.IssuePage({ issueId: issue.id }))
 
   return (
     <>
       <h1>Create A New Issue</h1>
       <div>
-        <NewIssueForm onSuccess={(issue) => router.push(Routes.IssuePage({ issueId: issue.id }))} />
+        <IssueForm
+          onSubmit={async (values) => {
+            try {
+              const issue = await createIssueMutation(values)
+              onSuccess(issue)
+            } catch (error) {
+              console.log(error)
+            }
+          }}
+          initialValues={initialValues}
+          submitText="Create Issue"
+        />
       </div>
     </>
   )
