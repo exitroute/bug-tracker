d1b6b286c76ff0ddb1a94acb1c0bf1d49e5bbd21
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-19 01:29:30 +0100

    add editForm

--- app/pages/issues/[issueId]/edit.tsx
+++ app/pages/issues/[issueId]/edit.tsx
@@ -1,15 +1,47 @@
 import { Suspense } from "react"
-import { useRouter, useParam, useMutation, BlitzPage } from "blitz"
+import { BlitzPage, useRouter, useParam, useMutation, useQuery } from "blitz"
 
-export const EditIssue = () => {
-  return <h1>Edit</h1>
+import { FORM_ERROR } from "app/issues/components/Form"
+import { IssueForm } from "app/issues/components/IssueForm"
+
+import getIssue from "app/issues/queries/getIssue"
+import updateIssue from "app/issues/mutations/updateIssue"
+
+export const EditIssueForm = () => {
+  const router = useRouter()
+  const issueId = useParam("issueId", "number")!
+  const [issue, { setQueryData }] = useQuery(getIssue, issueId, { staleTime: Infinity })
+  const [updateIssueMutation] = useMutation(updateIssue)
+
+  return (
+    <>
+      <h1>Edit Issue: {issue.title}</h1>
+      <pre>{JSON.stringify(issue, null, 2)}</pre>
+      <IssueForm
+        submitText="Update Issue"
+        initialValues={issue}
+        onSubmit={async (values) => {
+          try {
+            const updated = await updateIssueMutation({
+              id: issue.id,
+              ...values,
+            })
+            await setQueryData(updated)
+          } catch (error: any) {
+            console.log("Edit Issue", error)
+            return { [FORM_ERROR]: error.toString() }
+          }
+        }}
+      />
+    </>
+  )
 }
 
-const EditIssuePage = () => {
+const EditIssuePage: BlitzPage = () => {
   return (
     <>
       <Suspense fallback="Loading...">
-        <EditIssue />
+        <EditIssueForm />
       </Suspense>
     </>
   )
