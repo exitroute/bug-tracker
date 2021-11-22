f1bca1064baffe58845a3d16a17c185760a7bf2e
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-22 23:32:50 +0100

    make conditional rendering explicit

--- app/core/components/DetailsHeader.tsx
+++ app/core/components/DetailsHeader.tsx
@@ -2,17 +2,33 @@ import { Link, useParam } from "blitz"
 
 const DetailsHeader = ({ title }) => {
   const issueId = useParam("issueId", "number")!
+  console.log(title)
   return (
     <header>
-      {title === "Issue" ? (
-        <Link href={"/issues"}>
-          <a>Back</a>
-        </Link>
-      ) : (
-        <Link href={`/issues/${issueId}`}>
-          <a>Back</a>
-        </Link>
-      )}
+      {(() => {
+        switch (title) {
+          case "Issue":
+            return (
+              <Link href={"/issues"}>
+                <a>Back</a>
+              </Link>
+            )
+          case "Edit":
+            return (
+              <Link href={`/issues/${issueId}`}>
+                <a>Back</a>
+              </Link>
+            )
+          case "New":
+            return (
+              <Link href={`/home`}>
+                <a>Back</a>
+              </Link>
+            )
+          default:
+            return null
+        }
+      })()}
       <br />
       {title === "Issue" ? (
         <Link href={`/issues/${issueId}/edit`}>
