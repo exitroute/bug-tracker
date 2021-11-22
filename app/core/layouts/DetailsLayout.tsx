4279662dded489270056992b565340ecc9df408f
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-22 19:49:53 +0100

    add details layout

---
+++ app/core/layouts/DetailsLayout.tsx
@@ -0,0 +1,23 @@
+import { Head, BlitzLayout } from "blitz"
+import React, { Suspense } from "react"
+import styles from "./DetailsLayout.module.css"
+import EditIssueButton from "app/issues/components/EditIssueButton"
+
+const DetailsLayout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
+  return (
+    <div className={styles.wrapper}>
+      <Head>
+        <title>{title || "bug-tracker"}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <a href="">Back</a>
+        <Suspense fallback="Loading...">{title === "Issue" ? <EditIssueButton /> : null}</Suspense>
+      </header>
+
+      <div className={styles.content}>{children}</div>
+    </div>
+  )
+}
+
+export default DetailsLayout
