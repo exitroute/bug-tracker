1501b74d6bbeb48bc3737decf80371ef62d9b29f
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-23 14:52:14 +0100

    add users to form

--- app/issues/components/IssueForm.tsx
+++ app/issues/components/IssueForm.tsx
@@ -1,20 +1,40 @@
+import { Suspense } from "react"
+import { useQuery } from "blitz"
 import { Field } from "react-final-form"
 import { Form } from "./Form"
 export { FORM_ERROR } from "./Form"
+// todo get users and add to field with users
+import getUsers from "app/users/queries/getUsers"
 
 export const IssueForm = (props) => {
+  const [users] = useQuery(getUsers, undefined, { suspense: false })
+  console.log(users)
+
   return (
-    <Form {...props}>
-      <div>
-        <label>Title</label>
-        <br />
-        <Field name="title" component="input" type="text" placeholder="Add a descriptive title" />
-      </div>
-      <div>
-        <label>Expected behavior</label>
-        <br />
-        <Field name="description" component="textarea" placeholder="What happened?" />
-      </div>
-    </Form>
+    <Suspense fallback="Loading...">
+      <Form {...props}>
+        <div>
+          <label>Title</label>
+          <br />
+          <Field name="title" component="input" type="text" placeholder="Add a descriptive title" />
+        </div>
+        <div>
+          <label>Expected behavior</label>
+          <br />
+          <Field name="description" component="textarea" placeholder="What happened?" />
+        </div>
+        <div>
+          <label htmlFor="assign-user">Assigned to</label>
+          <br />
+          <Field name="users" component="select" multiple id="assign-user">
+            {users?.map((user) => (
+              <option key={user.id} value={user.id}>
+                {user.name}
+              </option>
+            ))}
+          </Field>
+        </div>
+      </Form>
+    </Suspense>
   )
 }
