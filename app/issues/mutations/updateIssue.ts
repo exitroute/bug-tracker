5db1b5878f1ddebc1d939226e95cb39f2bfa1acc
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-24 01:57:08 +0100

    add updated data to mutation

--- app/issues/mutations/updateIssue.ts
+++ app/issues/mutations/updateIssue.ts
@@ -2,13 +2,24 @@ import { Ctx } from "blitz"
 import db from "db"
 
 export default async function updateIssue({ id, ...data }, ctx: Ctx) {
-  console.log("data", data)
   ctx.session.$authorize()
+  console.log("##", data)
   const issue = await db.issue.update({
     where: { id },
     data: {
       title: data.title,
       description: data.description,
+      updatedBy: {
+        connect: {
+          id: ctx.session.userId,
+        },
+      },
+      updatedAt: new Date().toISOString(),
+      // assignedTo: {
+      //   connect: {
+      //     id: Number(data.assignedTo[0]),
+      //   },
+      // },
     },
     // add an updateBy and when record
   })
