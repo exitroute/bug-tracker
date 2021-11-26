fce32058177bcd26235d9dac8fb7ac5425f1ecc9
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-11-26 21:15:14 +0100

    remove logging

--- app/issues/components/Form.tsx
+++ app/issues/components/Form.tsx
@@ -2,9 +2,10 @@ import { Form as FinalForm } from "react-final-form"
 export { FORM_ERROR } from "final-form"
 
 export const Form = ({ children, ...props }) => {
+  console.log(props.initialValues.issue)
   return (
     <FinalForm
-      initialValues={props.initialValues}
+      initialValues={props.initialValues.issue}
       onSubmit={props.onSubmit}
       render={({ handleSubmit, form, submitting, pristine, submitError }) => (
         <form id="issue-form" onSubmit={handleSubmit}>
