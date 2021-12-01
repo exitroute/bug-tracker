8749627a6ebef7ba0119080bdae0e7a97bc0276d
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-01 19:18:27 +0100

    add edit form and mutation

---
+++ app/projects/mutations/updateProject.ts
@@ -0,0 +1,29 @@
+import { Ctx } from "blitz"
+import db from "db"
+
+export default async function updateProject({ id, ...data }, ctx: Ctx) {
+  ctx.session.$authorize("ADMIN")
+  try {
+    const { project } = data
+    const updatedProject = await db.project.update({
+      where: { id },
+      data: {
+        title: project.title,
+        description: project.description,
+        updatedBy: {
+          connect: {
+            id: ctx.session.userId,
+          },
+        },
+        //   assignedTo: {
+        //     connect: {
+        //       id: Number(project.assignedTo.id),
+        //     },
+        //   },
+      },
+    })
+    return updatedProject
+  } catch (error) {
+    console.error("UPDATE ISSUE ERROR", error)
+  }
+}
---
+++ app/projects/pages/projects/[projectId]/edit.tsx
@@ -0,0 +1,69 @@
+import { Suspense } from "react"
+import { BlitzPage, Routes, useRouter, useParam, useMutation, useQuery } from "blitz"
+
+import { Flex, Stack, Heading, Text, useColorModeValue } from "@chakra-ui/react"
+
+import DetailsLayout from "app/core/layouts/DetailsLayout"
+import { FORM_ERROR } from "app/core/components/AppForm"
+import { ProjectForm } from "app/projects/components/ProjectForm"
+
+import getProject from "app/projects/queries/getProject"
+import updateProject from "app/projects/mutations/updateProject"
+
+export const EditProjectForm = () => {
+  const router = useRouter()
+  const projectId = useParam("projectId", "number")!
+
+  const [project] = useQuery(getProject, projectId, { suspense: false, staleTime: Infinity })
+  const initialValues = { project }
+
+  const redirect = (updated) => router.push(Routes.ProjectPage({ projectId: updated.id }))
+  const [updateProjectMutation] = useMutation(updateProject, { onSuccess: redirect })
+
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading>Edit Project: {project?.title}</Heading>
+          <Text fontSize={"lg"} color={"gray.600"}>
+            What do you want to change?
+          </Text>
+        </Stack>
+        <ProjectForm
+          initialValues={initialValues}
+          submitText="Update Project"
+          onSubmit={async (values) => {
+            try {
+              await updateProjectMutation({
+                id: project?.id,
+                ...values,
+              })
+            } catch (error: any) {
+              return { [FORM_ERROR]: error.toString() }
+            }
+          }}
+        />
+      </Stack>
+    </Flex>
+  )
+}
+
+const EditProjectPage: BlitzPage = () => {
+  return (
+    <>
+      <Suspense fallback="Loading...">
+        <EditProjectForm />
+      </Suspense>
+    </>
+  )
+}
+
+EditProjectPage.authenticate = true
+EditProjectPage.suppressFirstRenderFlicker = true
+EditProjectPage.getLayout = (page) => <DetailsLayout title="Edit">{page}</DetailsLayout>
+export default EditProjectPage
