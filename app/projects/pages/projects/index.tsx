4fdb5a8def10d80df627dfeb9e2e1a3a83ef08a7
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-01 12:13:24 +0100

    testing layout

--- app/projects/pages/projects/index.tsx
+++ app/projects/pages/projects/index.tsx
@@ -1,9 +1,22 @@
 import { BlitzPage } from "next"
 
 import Layout from "app/core/layouts/Layout"
+import { Box } from "@chakra-ui/layout"
 
 const Projects: BlitzPage = () => {
-  return <h1>Your Projects</h1>
+  return (
+    <Box as="main" h="100%" pos="relative">
+      <div>
+        <ul>
+          <li>New projects!</li>
+          <li>New projects!</li>
+          <li>New projects!</li>
+          <li>New projects!</li>
+          <li>New projects!</li>
+        </ul>
+      </div>
+    </Box>
+  )
 }
 
 Projects.authenticate = true
