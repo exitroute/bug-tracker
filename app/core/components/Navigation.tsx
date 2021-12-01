87e48a5e14a8280d39df0c7c31c38daea2edc0d2
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-01 12:11:24 +0100

    fix layout

--- app/core/components/Navigation.tsx
+++ app/core/components/Navigation.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from "react"
 import { Link } from "blitz"
-import { Box, Flex } from "@chakra-ui/react"
+import { Flex } from "@chakra-ui/react"
 
 const routes = ["Home", "Issues", "Projects", "Users", "Teams"]
 
 const Navigation = () => {
   return (
-    <Flex as="nav" justify="space-around" alignItems="flex-end" h="100%" pb="4" pt="4">
+    <Flex as="nav" justify="space-around" align="center" h="4rem">
       {routes.map((route, index) => (
         <Link key={index} href={`/${route.toLowerCase()}`}>
           <a>{route}</a>
