4eabb581610b55192597758164705a5fc018982f
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-02 17:46:43 +0100

    make nav button respond to page

--- app/core/components/DetailsHeader.tsx
+++ app/core/components/DetailsHeader.tsx
@@ -1,34 +1,54 @@
-import { Link, useParam } from "blitz"
+import { Link, useParam, useRouter } from "blitz"
+import { Box } from "@chakra-ui/react"
 
-const DetailsHeader = ({ title }) => {
-  const issueId = useParam("issueId", "number")!
+export const BackButton = ({ title }) => {
+  const router = useRouter()
+  let slashes = new Array()
+  let letters = new Array()
+
+  router.asPath.split("").map((el, i) => {
+    if (slashes.length < 2) el === "/" ? slashes.push(`${el}`) : letters.push(`${el}`)
+    else return
+  })
+
+  const path = letters.join("")
+  const singular = path.substring(0, path.length - 1)
+  const capitalized = singular.charAt(0).toUpperCase() + singular.slice(1)
+
+  const id = useParam(`${singular}Id`, "number")!
+
+  if (title === capitalized) {
+    return (
+      <Link href={`/${path}`}>
+        <a>{capitalized + "s"}</a>
+      </Link>
+    )
+  } else if (title === "Edit") {
+    return (
+      <Link href={`/${path}/${id}`}>
+        <a>Back</a>
+      </Link>
+    )
+  } else if (title === "New") {
+    return (
+      <Link href={`/home`}>
+        <a>Home</a>
+      </Link>
+    )
+  } else {
+    return (
+      <Link href={`/home`}>
+        <a>Home</a>
+      </Link>
+    )
+  }
+}
+
+export const DetailsHeader = ({ title }) => {
   return (
-    <header>
-      {(() => {
-        switch (title) {
-          case "Issue":
-            return (
-              <Link href={"/issues"}>
-                <a>Back</a>
-              </Link>
-            )
-          case "Edit":
-            return (
-              <Link href={`/issues/${issueId}`}>
-                <a>Back</a>
-              </Link>
-            )
-          case "New":
-            return (
-              <Link href={`/home`}>
-                <a>Back</a>
-              </Link>
-            )
-          default:
-            return null
-        }
-      })()}
-    </header>
+    <Box as="header" p="1rem">
+      <BackButton title={title} />
+    </Box>
   )
 }
 
