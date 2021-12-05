6b20f9e7a5b83dd6af99794b1c1e2b33c8dec435
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-06 00:59:55 +0100

    add mutators

--- app/core/components/AppForm.tsx
+++ app/core/components/AppForm.tsx
@@ -1,4 +1,6 @@
 import { Form as FinalForm } from "react-final-form"
+import arrayMutators from "final-form-arrays"
+
 import { Flex, Button } from "@chakra-ui/react"
 export { FORM_ERROR } from "final-form"
 
@@ -6,11 +8,23 @@ export const Form = ({ children, ...props }) => {
   return (
     <FinalForm
       initialValues={props.initialValues}
+      mutators={{
+        ...arrayMutators,
+      }}
       onSubmit={props.onSubmit}
-      render={({ handleSubmit, form, submitting, pristine, submitError }) => (
+      render={({
+        handleSubmit,
+        form: {
+          mutators: { push, pop },
+        },
+        form,
+        submitting,
+        pristine,
+        submitError,
+        values,
+      }) => (
         <form id="issue-form" onSubmit={handleSubmit}>
           {children}
-
           {props.submitError && (
             <div role="alert" style={{ color: "red" }}>
               {submitError}
