17e56a4760a37c35fad55a3d1a3fa30e7a76201d
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-06 01:47:13 +0100

    removed for debugging

--- app/teams/mutations/updateTeam.ts
+++ app/teams/mutations/updateTeam.ts
@@ -10,14 +10,11 @@ export default async function updateTeam({ id, ...data }, ctx: Ctx) {
       data: {
         title: team.title,
         description: team.description,
-        // updatedBy: {
-        //   connect: {
-        //     id: ctx.session.userId,
-        //   },
-        // },
-        // members: {
-        //   set: team.members,
-        // },
+        updatedBy: {
+          connect: {
+            id: ctx.session.userId,
+          },
+        },
       },
     })
     return updatedTeam
