48e590622207450112353d7acb79df6c3f018583
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-07 00:56:07 +0100

    bug fix

--- app/issues/mutations/updateIssue.ts
+++ app/issues/mutations/updateIssue.ts
@@ -1,25 +1,39 @@
 import { Ctx } from "blitz"
 import db from "db"
 
-export default async function updateIssue(data: any, ctx: Ctx) {
+export default async function updateIssue(input: any, ctx: Ctx) {
   ctx.session.$authorize()
+
+  const {
+    id,
+    title,
+    description,
+    assignedTo,
+  }: {
+    id: number
+    title: string
+    description: string
+    assignedTo: any
+  } = input.issue
+
   try {
-    const { issue } = data
     const updatedIssue = await db.issue.update({
-      where: { id: issue.id },
+      where: { id },
       data: {
-        title: issue.title,
-        description: issue.description,
+        title: title,
+        description: description,
         updatedBy: {
           connect: {
             id: ctx.session.userId,
           },
         },
-        assignedTo: {
-          connect: {
-            id: Number(issue.assignedTo.id),
+        ...(assignedTo && {
+          assignedTo: {
+            connect: {
+              id: Number(assignedTo.id),
+            },
           },
-        },
+        }),
       },
     })
     return updatedIssue
