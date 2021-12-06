d6085af8ff50e04847ca92adf999eeded5491a59
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-06 01:25:58 +0100

    add error handling

--- app/issues/mutations/createIssue.ts
+++ app/issues/mutations/createIssue.ts
@@ -3,22 +3,25 @@ import db from "db"
 
 export default async function createIssue(input: any, ctx: Ctx) {
   ctx.session.$authorize()
-  const newIssue = await db.issue.create({
-    data: {
-      title: input.issue.title,
-      description: input.issue.description,
-      createdBy: {
-        connect: {
-          id: ctx.session.userId,
+  try {
+    const newIssue = await db.issue.create({
+      data: {
+        title: input.issue.title,
+        description: input.issue.description,
+        createdBy: {
+          connect: {
+            id: ctx.session.userId,
+          },
+        },
+        assignedTo: {
+          connect: {
+            id: Number(input.issue.assignedTo.id),
+          },
         },
       },
-      assignedTo: {
-        connect: {
-          id: Number(input.issue.assignedTo.id),
-        },
-      },
-    },
-  })
-
-  return newIssue
+    })
+    return newIssue
+  } catch (error) {
+    console.error("CREATE ISSUE ERROR", error)
+  }
 }
