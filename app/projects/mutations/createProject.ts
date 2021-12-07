a9d438480ec77576ba114c36ba7ca26a1a471f05
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-07 01:26:27 +0100

    Fix bug

--- app/projects/mutations/createProject.ts
+++ app/projects/mutations/createProject.ts
@@ -3,22 +3,38 @@ import db from "db"
 
 export default async function createProject(input: any, ctx: Ctx) {
   ctx.session.$authorize("ADMIN")
-  const newProject = await db.project.create({
-    data: {
-      title: input.project.title,
-      description: input.project.description,
-      createdBy: {
-        connect: {
-          id: ctx.session.userId,
-        },
-      },
-      assignedIssues: {
-        connect: {
-          id: Number(input.project.assignedTo.id),
-        },
-      },
-    },
-  })
 
-  return newProject
+  const {
+    title,
+    description,
+    assignedIssue,
+  }: {
+    title: string
+    description: string
+    assignedIssue: any
+  } = input.project
+
+  try {
+    const newProject = await db.project.create({
+      data: {
+        title: title,
+        description: description,
+        createdBy: {
+          connect: {
+            id: ctx.session.userId,
+          },
+        },
+        ...(assignedIssue && {
+          assignedIssues: {
+            connect: {
+              id: Number(assignedIssue.id),
+            },
+          },
+        }),
+      },
+    })
+    return newProject
+  } catch (error) {
+    console.error("CREATE PROJECT ERROR", error)
+  }
 }
