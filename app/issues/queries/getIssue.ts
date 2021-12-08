352d783dd292148b06c0992104c4842c321b29ce
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-09 00:40:40 +0100

    at try catch to getIssue

--- app/issues/queries/getIssue.ts
+++ app/issues/queries/getIssue.ts
@@ -2,29 +2,33 @@ import { NotFoundError } from "blitz"
 import db from "db"
 
 export default async function getIssue(id: number) {
-  const issue = await db.issue.findUnique({
-    where: { id: id },
-    include: {
-      createdBy: {
-        select: {
-          name: true,
+  try {
+    const issue = await db.issue.findUnique({
+      where: { id: id },
+      include: {
+        createdBy: {
+          select: {
+            name: true,
+          },
+        },
+        updatedBy: {
+          select: {
+            name: true,
+          },
+        },
+        assignedTo: {
+          select: {
+            id: true,
+            name: true,
+          },
         },
       },
-      updatedBy: {
-        select: {
-          name: true,
-        },
-      },
-      assignedTo: {
-        select: {
-          id: true,
-          name: true,
-        },
-      },
-    },
-  })
-  if (!issue) {
-    throw new NotFoundError()
+    })
+    if (!issue) {
+      throw new NotFoundError()
+    }
+    return issue
+  } catch (error) {
+    console.error("GET ISSUE ERROR", error)
   }
-  return issue
 }
