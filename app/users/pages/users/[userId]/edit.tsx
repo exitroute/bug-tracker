a3b53f37ba99de23a80f033918bf4d9b95fc5f51
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-09 00:47:08 +0100

    add get user page

--- app/users/pages/users/[userId]/edit.tsx
+++ app/users/pages/users/[userId]/edit.tsx
@@ -1,12 +1,70 @@
-/**
- * this is where a user can edit their profile
- *
- */
+import { Suspense } from "react"
+import { BlitzPage, Routes, useRouter, useParam, useMutation, useQuery } from "blitz"
 
-import { BlitzPage } from "next"
+import { Flex, Stack, Heading, Text, useColorModeValue } from "@chakra-ui/react"
 
-const EditProfile: BlitzPage = () => {
-  return <h1>Edit Your Profile</h1>
+import DetailsLayout from "app/core/layouts/DetailsLayout"
+import { FORM_ERROR } from "app/core/components/AppForm"
+import { UserProfileForm } from "app/users/components/UserProfileForm"
+
+import getInitialUserProfileData from "app/users/queries/getInitialUserProfileData"
+import updateUserProfile from "app/users/mutations/updateUserProfile"
+
+export const EditUserProfileForm = () => {
+  const router = useRouter()
+  const userId = useParam("userId", "number")!
+
+  const [userProfile] = useQuery(getInitialUserProfileData, userId, {
+    suspense: false,
+    staleTime: Infinity,
+  })
+  const initialValues = { userProfile }
+
+  const redirect = (updated) => router.push(Routes.UserProfilePage({ userId: updated.id }))
+  const [updateUserProfileMutation] = useMutation(updateUserProfile, { onSuccess: redirect })
+
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading>Edit Issue: {userProfile?.name}</Heading>
+          <Text fontSize={"lg"} color={"gray.600"}>
+            What do you want to change?
+          </Text>
+        </Stack>
+        <UserProfileForm
+          initialValues={initialValues}
+          submitText="Update Issue"
+          onSubmit={async (values) => {
+            try {
+              await updateUserProfileMutation(values)
+            } catch (error: any) {
+              return { [FORM_ERROR]: error.toString() }
+            }
+          }}
+        />
+      </Stack>
+    </Flex>
+  )
 }
 
-export default EditProfile
+const EditUserProfilePage: BlitzPage = () => {
+  return (
+    <>
+      <Suspense fallback="Loading...">
+        <EditUserProfileForm />
+      </Suspense>
+    </>
+  )
+}
+
+EditUserProfilePage.authenticate = true
+EditUserProfilePage.suppressFirstRenderFlicker = true
+EditUserProfilePage.getLayout = (page) => <DetailsLayout title="Edit">{page}</DetailsLayout>
+
+export default EditUserProfilePage
