e63bd65f2c353a1ef30b4ebea3a7b2866e09f3a8
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-09 00:46:10 +0100

    add userProfile page

--- app/users/pages/users/[userId].tsx
+++ app/users/pages/users/[userId].tsx
@@ -1,11 +1,86 @@
-/**
- * This is where a user sees a specific user profile
- */
+import { Suspense } from "react"
+import { BlitzPage, useParam, useQuery, useMutation, Link, Router } from "blitz"
 
-import { BlitzPage } from "next"
+import { Box, Flex, Button, useColorModeValue } from "@chakra-ui/react"
 
-const UserProfílePage: BlitzPage = () => {
-  return <h1>User Profile</h1>
+import {
+  Card,
+  Property,
+  CardHeader,
+  CardFooter,
+  CardContent,
+} from "app/core/components/CardComponents"
+
+import DetailsLayout from "app/core/layouts/DetailsLayout"
+import getUserProfile from "app/users/queries/getUserProfile"
+
+const UserProfileDetails = () => {
+  const userId = useParam("userId", "number")!
+  const [user, { refetch }]: any = useQuery(getUserProfile, userId)
+
+  const { id, name, email, role, assignedIssues, assignedProjects, inTeams } = user
+
+  const deleteUserProfileHandler = (e, id) => {
+    e.preventDefault()
+    // deleteUserProfileMutation({ id })
+    confirm("Warning: You are about to delete this profile. \nAre you sure?")
+    Router.push("/issues")
+    refetch()
+  }
+
+  return (
+    <Box as="main" height="100%" bg={useColorModeValue("gray.100", "inherit")}>
+      <Flex as="section" minH={"100vh"} align={"center"} justify={"center"}>
+        <Card maxW="3xl" mx="auto">
+          <CardHeader
+            title={`${name}`}
+            action={
+              <Link href={`/users/${userId}/edit`}>
+                <Button as="a" variant="outline" minW="20">
+                  Edit
+                </Button>
+              </Link>
+            }
+          />
+          <CardContent>
+            <Property label="Role" value={`${role}`} />
+            <Property label="Email" value={`${email}`} />
+            {assignedIssues &&
+              assignedIssues.map((issue, index) => (
+                <Property key={index} label="Issue" value={`${issue.title}`} />
+              ))}
+            {assignedProjects &&
+              assignedProjects.map((project, index) => (
+                <Property key={index} label="Project" value={`${project.title}`} />
+              ))}
+            {inTeams &&
+              inTeams.map((team, index) => (
+                <Property key={index} label="Team" value={`${team.title}`} />
+              ))}
+          </CardContent>
+          <CardFooter
+            action={
+              <Button colorScheme="pink" onClick={(e) => deleteUserProfileHandler(e, id)}>
+                Delete
+              </Button>
+            }
+          />
+        </Card>
+      </Flex>
+    </Box>
+  )
 }
 
-export default UserProfílePage
+const UserProfilePage: BlitzPage = () => {
+  return (
+    <Suspense fallback="Loading...">
+      <UserProfileDetails />
+    </Suspense>
+  )
+}
+
+UserProfilePage.authenticate = true
+UserProfilePage.suppressFirstRenderFlicker = true
+UserProfilePage.getLayout = (page) => <DetailsLayout title="Profile">{page}</DetailsLayout>
+
+export default UserProfilePage
