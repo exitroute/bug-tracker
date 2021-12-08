100b655c61f9b505ae603f1f34afc93bd61e50e4
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-09 00:41:19 +0100

    move authorisation in deleteProject

--- app/projects/mutations/deleteProject.ts
+++ app/projects/mutations/deleteProject.ts
@@ -2,8 +2,9 @@ import { Ctx } from "blitz"
 import db from "db"
 
 export default async function deleteProject({ id }, ctx: Ctx) {
+  ctx.session.$authorize("ADMIN")
+
   try {
-    ctx.session.$authorize("ADMIN")
     const project = await db.project.delete({
       where: { id },
     })
