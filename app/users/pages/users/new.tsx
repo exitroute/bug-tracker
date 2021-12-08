875d74bd857fe6bbf5dba2358911de4634539619
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-09 00:46:50 +0100

    add creat user page

--- app/users/pages/users/new.tsx
+++ app/users/pages/users/new.tsx
@@ -1,11 +1,48 @@
-/**
- * This is where the user arrives after signing up
- */
+import { BlitzPage, Router, useMutation } from "blitz"
 
-import { BlitzPage } from "next"
+import { Flex, Stack, Heading, Text, useColorModeValue } from "@chakra-ui/react"
 
-const NewProfile: BlitzPage = () => {
-  return <h1>Create Your Profile</h1>
+import DetailsLayout from "app/core/layouts/DetailsLayout"
+import { UserProfileForm } from "app/users/components/UserProfileForm"
+import createUser from "app/users/mutations/createUser"
+
+const NewUserPage: BlitzPage = () => {
+  const initialValues = { userProfile: { name: "", about: "" } }
+
+  const [createUserMutation] = useMutation(createUser)
+
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading>New User</Heading>
+          <Text fontSize={"lg"} color={"gray.600"}>
+            Who is joining the team?
+          </Text>
+        </Stack>
+        <UserProfileForm
+          onSubmit={async (values) => {
+            try {
+              const issue = await createUserMutation(values)
+              Router.push(`/user/${user?.id}`)
+            } catch (error) {
+              console.error("CREATE USERPROFILE MUTATION ERROR", error)
+            }
+          }}
+          initialValues={initialValues}
+          submitText="Create User"
+        />
+      </Stack>
+    </Flex>
+  )
 }
 
-export default NewProfile
+NewUserPage.authenticate = true
+NewUserPage.suppressFirstRenderFlicker = true
+NewUserPage.getLayout = (page) => <DetailsLayout title="New">{page}</DetailsLayout>
+export default NewUserPage
