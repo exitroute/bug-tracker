3c6041481d89b058ee60381a152bb7faefcf731d
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-09 00:41:54 +0100

    add userProfile form

---
+++ app/users/components/UserProfileForm.tsx
@@ -0,0 +1,41 @@
+import { Suspense } from "react"
+import { useQuery } from "blitz"
+
+import { InputControl, TextareaControl, SelectControl } from "app/core/components/FormComponents"
+
+import { Box, Stack, useColorModeValue } from "@chakra-ui/react"
+
+import { Form } from "../../core/components/AppForm"
+export { FORM_ERROR } from "../../core/components/AppForm"
+
+// import getUsers from "app/users/queries/getUserProfiles"
+
+export const UserProfileForm = (props) => {
+  // const [users] = useQuery(getUsers, undefined, { suspense: false })
+
+  return (
+    <Suspense fallback="Loading...">
+      <Box rounded={"lg"} bg={useColorModeValue("white", "gray.700")} boxShadow={"lg"} p={8}>
+        <Stack spacing={4}>
+          <Form {...props}>
+            <Stack spacing={8}>
+              <InputControl name="userProfile.name" placeholder="Your name" label="Name" />
+              <InputControl name="userProfile.email" placeholder="Your email" label="Email" />
+              {/* <SelectControl
+                name="issue.assignedTo.id"
+                label="Assigned to"
+                placeholder="Assign this issue!"
+              >
+                {users?.map((user) => (
+                  <option key={user.id} value={user.id}>
+                    {user.name}
+                  </option>
+                ))}
+              </SelectControl> */}
+            </Stack>
+          </Form>
+        </Stack>
+      </Box>
+    </Suspense>
+  )
+}
