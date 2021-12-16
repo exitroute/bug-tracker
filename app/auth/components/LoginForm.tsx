e698aac0880b271e562e7df3ae675ef840bc6d02
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-16 19:21:01 +0100

    add layouts to form components gives component style out side of page

--- app/auth/components/LoginForm.tsx
+++ app/auth/components/LoginForm.tsx
@@ -1,6 +1,7 @@
 import { AuthenticationError, Link, useMutation, Routes, PromiseReturnType } from "blitz"
 import { LabeledTextField } from "app/core/components/LabeledTextField"
 import { Form, FORM_ERROR } from "app/core/components/Form"
+import { LoginLayout } from "app/core/components/AuthFormComponents"
 import login from "app/auth/mutations/login"
 import { Login } from "app/auth/validations"
 
@@ -12,34 +13,36 @@ export const LoginForm = (props: LoginFormProps) => {
   const [loginMutation] = useMutation(login)
 
   return (
-    <Form
-      submitText="Login"
-      schema={Login}
-      initialValues={{ email: "", password: "" }}
-      onSubmit={async (values) => {
-        try {
-          const user = await loginMutation(values)
-          props.onSuccess?.(user)
-        } catch (error: any) {
-          if (error instanceof AuthenticationError) {
-            return { [FORM_ERROR]: "Sorry, those credentials are invalid" }
-          } else {
-            return {
-              [FORM_ERROR]:
-                "Sorry, we had an unexpected error. Please try again. - " + error.toString(),
+    <LoginLayout>
+      <Form
+        submitText="Login"
+        schema={Login}
+        initialValues={{ email: "", password: "" }}
+        onSubmit={async (values) => {
+          try {
+            const user = await loginMutation(values)
+            props.onSuccess?.(user)
+          } catch (error: any) {
+            if (error instanceof AuthenticationError) {
+              return { [FORM_ERROR]: "Sorry, those credentials are invalid" }
+            } else {
+              return {
+                [FORM_ERROR]:
+                  "Sorry, we had an unexpected error. Please try again. - " + error.toString(),
+              }
             }
           }
-        }
-      }}
-    >
-      <LabeledTextField name="email" label="Email" placeholder="Email" />
-      <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
-      <div>
-        <Link href={Routes.ForgotPasswordPage()}>
-          <a>Forgot your password?</a>
-        </Link>
-      </div>
-    </Form>
+        }}
+      >
+        <LabeledTextField name="email" label="Email" placeholder="Email" />
+        <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
+        <div>
+          <Link href={Routes.ForgotPasswordPage()}>
+            <a>Forgot your password?</a>
+          </Link>
+        </div>
+      </Form>
+    </LoginLayout>
   )
 }
 
--- app/auth/components/SignupForm.tsx
+++ app/auth/components/SignupForm.tsx
@@ -1,6 +1,7 @@
 import { useMutation } from "blitz"
 import { LabeledTextField } from "app/core/components/LabeledTextField"
 import { Form, FORM_ERROR } from "app/core/components/Form"
+import { SignUpLayout } from "app/core/components/AuthFormComponents"
 import signup from "app/auth/mutations/signup"
 import { Signup } from "app/auth/validations"
 
@@ -12,27 +13,29 @@ export const SignupForm = (props: SignupFormProps) => {
   const [signupMutation] = useMutation(signup)
 
   return (
-    <Form
-      submitText="Create Account"
-      schema={Signup}
-      initialValues={{ email: "", password: "" }}
-      onSubmit={async (values) => {
-        try {
-          await signupMutation(values)
-          props.onSuccess?.()
-        } catch (error: any) {
-          if (error.code === "P2002" && error.meta?.target?.includes("email")) {
-            // This error comes from Prisma
-            return { email: "This email is already being used" }
-          } else {
-            return { [FORM_ERROR]: error.toString() }
+    <SignUpLayout>
+      <Form
+        submitText="Create Account"
+        schema={Signup}
+        initialValues={{ email: "", password: "" }}
+        onSubmit={async (values) => {
+          try {
+            await signupMutation(values)
+            props.onSuccess?.()
+          } catch (error: any) {
+            if (error.code === "P2002" && error.meta?.target?.includes("email")) {
+              // This error comes from Prisma
+              return { email: "This email is already being used" }
+            } else {
+              return { [FORM_ERROR]: error.toString() }
+            }
           }
-        }
-      }}
-    >
-      <LabeledTextField name="email" label="Email" placeholder="Email" />
-      <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
-    </Form>
+        }}
+      >
+        <LabeledTextField name="email" label="Email" placeholder="Email" />
+        <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
+      </Form>
+    </SignUpLayout>
   )
 }
 
