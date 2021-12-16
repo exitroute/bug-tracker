9d8ee7745daec85bde6376a83beb31f6a76689fe
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-16 17:17:12 +0100

    Add auth form chakra components

---
+++ app/core/components/AuthFormComponents.tsx
@@ -0,0 +1,73 @@
+import {
+  Box,
+  Heading,
+  Text,
+  BoxProps,
+  useColorModeValue,
+  chakra,
+  HTMLChakraProps,
+} from "@chakra-ui/react"
+
+export const SignUpLayout = ({ children }) => (
+  <Box
+    bg={useColorModeValue("gray.50", "inherit")}
+    minH="100vh"
+    py="12"
+    px={{ base: "4", lg: "8" }}
+  >
+    <Box maxW="md" mx="auto">
+      <Heading textAlign="center" size="xl" fontWeight="extrabold">
+        Sign up
+      </Heading>
+      <Text mt="4" mb="8" align="center" maxW="md" fontWeight="medium">
+        <Text as="span">Creating an account is free!</Text>
+        <br />
+        <Link href="./login">Already have an account?</Link>
+      </Text>
+      <Card>{children}</Card>
+    </Box>
+  </Box>
+)
+
+export const LoginLayout = ({ children }) => (
+  <Box
+    bg={useColorModeValue("gray.50", "inherit")}
+    minH="100vh"
+    py="12"
+    px={{ base: "4", lg: "8" }}
+  >
+    <Box maxW="md" mx="auto">
+      <Heading textAlign="center" size="xl" fontWeight="extrabold">
+        Sign in to your account
+      </Heading>
+      <Text mt="4" mb="8" align="center" maxW="md" fontWeight="medium">
+        <Text as="span">Don&apos;t have an account?</Text>
+        <br />
+        <Link href="./signup">Creating an account is free!</Link>
+      </Text>
+      <Card>{children}</Card>
+    </Box>
+  </Box>
+)
+
+const Card = (props: BoxProps) => (
+  <Box
+    bg={useColorModeValue("white", "gray.700")}
+    py="8"
+    px={{ base: "4", md: "10" }}
+    shadow="base"
+    rounded={{ sm: "lg" }}
+    {...props}
+  />
+)
+
+const Link = (props: HTMLChakraProps<"a">) => (
+  <chakra.a
+    marginStart="1"
+    href="#"
+    color={useColorModeValue("blue.500", "blue.200")}
+    _hover={{ color: useColorModeValue("blue.600", "blue.300") }}
+    display={{ base: "block", sm: "inline" }}
+    {...props}
+  />
+)
