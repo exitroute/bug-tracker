5899a0795669a591c54ac9e6d162bd52e296b166
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-17 16:12:33 +0100

    issueId went to issue page with undefinded data removed try catch and 404 page is shown

--- app/issues/queries/getIssue.ts
+++ app/issues/queries/getIssue.ts
@@ -2,33 +2,29 @@ import { NotFoundError } from "blitz"
 import db from "db"
 
 export default async function getIssue(id: number) {
-  try {
-    const issue = await db.issue.findUnique({
-      where: { id: id },
-      include: {
-        createdBy: {
-          select: {
-            name: true,
-          },
-        },
-        updatedBy: {
-          select: {
-            name: true,
-          },
-        },
-        assignedTo: {
-          select: {
-            id: true,
-            name: true,
-          },
+  const issue = await db.issue.findUnique({
+    where: { id: id },
+    include: {
+      createdBy: {
+        select: {
+          name: true,
         },
       },
-    })
-    if (!issue) {
-      throw new NotFoundError()
-    }
-    return issue
-  } catch (error) {
-    console.error("GET ISSUE ERROR", error)
+      updatedBy: {
+        select: {
+          name: true,
+        },
+      },
+      assignedTo: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+    },
+  })
+  if (!issue) {
+    throw new NotFoundError()
   }
+  return issue
 }
