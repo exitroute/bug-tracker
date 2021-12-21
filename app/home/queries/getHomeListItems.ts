66a500cb097e0e62506243257372bdb92f84f13e
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-12-21 18:28:46 +0100

    Instantiate getHomeListFile

---
+++ app/home/queries/getHomeListItems.ts
@@ -0,0 +1,19 @@
+/**
+ *
+ *
+ *  * In the feed user sees their
+ *  active tickets
+ *  projects
+ *  team
+ *
+ * and any unassigned tickets
+ *
+ * The user can
+ *  go to their profile
+ *  go to search
+ *  go to users
+ *  go to tickets
+ *the
+ */
+
+export {}
