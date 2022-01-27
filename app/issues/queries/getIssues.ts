63690fd566f7aaf9c6d428d79c70f145fe5cd4a5
Ryan O'Shea <ryan.oshea@exitroute.org>
2022-01-28 00:13:04 +0100

    get assignedto name for issue cards

--- app/issues/queries/getIssues.ts
+++ app/issues/queries/getIssues.ts
@@ -2,7 +2,15 @@ import db from "db"
 
 export default async function getIssues() {
   try {
-    const issues = await db.issue.findMany()
+    const issues = await db.issue.findMany({
+      include: {
+        assignedTo: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    })
     return issues
   } catch (error) {
     console.error("getIssues Error ", error)
