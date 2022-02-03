137c89d0ee24a3fdcd500a295bcc579bc245f4f7
Ryan O'Shea <ryan.oshea@exitroute.org>
2022-02-03 23:33:20 +0100

    fix delete bug #13

--- app/issues/mutations/deleteIssue.ts
+++ app/issues/mutations/deleteIssue.ts
@@ -2,9 +2,14 @@ import { Ctx } from "blitz"
 import db from "db"
 
 export default async function deleteIssue({ id }, ctx: Ctx) {
-  ctx.session.$authorize()
-  const issue = await db.issue.delete({
-    where: { id },
-  })
-  return issue
+  ctx.session.$authorize("ADMIN")
+
+  try {
+    const issue = await db.issue.delete({
+      where: { id },
+    })
+    return issue
+  } catch (error) {
+    console.log("DELETE ISSUE ERROR", error)
+  }
 }
