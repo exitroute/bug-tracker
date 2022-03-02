a3e70fa4e0270a89a6bdabe8b7fa5d8099b5bc3e
Ryan O'Shea <ryan.oshea@exitroute.org>
2022-03-02 20:37:41 +0100

    add layout to settings placeholder

--- app/settings/pages/settings/index.tsx
+++ app/settings/pages/settings/index.tsx
@@ -1,9 +1,19 @@
 import { BlitzPage } from "next"
 
+import { Center, Flex, Heading } from "@chakra-ui/layout"
+
 import Layout from "app/core/layouts/Layout"
 
 const Settings: BlitzPage = () => {
-  return <h1>Your Settings</h1>
+  return (
+    <Flex direction="column" justifyContent="center" height="100%">
+      <Center>
+        <Heading as="h3" size="md" color="gray.500">
+          The settings page is under construction
+        </Heading>
+      </Center>
+    </Flex>
+  )
 }
 
 Settings.authenticate = { redirectTo: "/" }
