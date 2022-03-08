764834f7e126aaaf6fb05f38a74c03d04898868b
Ryan O'Shea <ryan.oshea@exitroute.org>
2022-03-08 22:27:48 +0100

    layout fix

--- app/issues/components/IssueForm.tsx
+++ app/issues/components/IssueForm.tsx
@@ -25,7 +25,7 @@ export const IssueForm = (props) => {
 
   return (
     <Suspense fallback="Loading...">
-      <Box rounded={"lg"} bg={useColorModeValue("white", "gray.700")} boxShadow={"lg"} p={8}>
+      <Box p={8}>
         <Stack spacing={4}>
           <Form {...props}>
             <Stack spacing={8}>
