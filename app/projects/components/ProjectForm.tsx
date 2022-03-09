ade886dc96f914516e87ed96d4174ad2bffaf858
Ryan O'Shea <ryan.oshea@exitroute.org>
2022-03-09 21:24:19 +0100

    remove layout styles

--- app/projects/components/ProjectForm.tsx
+++ app/projects/components/ProjectForm.tsx
@@ -16,7 +16,7 @@ export const ProjectForm = (props) => {
 
   return (
     <Suspense fallback="Loading...">
-      <Box rounded={"lg"} bg={useColorModeValue("white", "gray.700")} boxShadow={"lg"} p={8}>
+      <Box p={8}>
         <Stack spacing={4}>
           <Form {...props}>
             <Stack spacing={8}>
