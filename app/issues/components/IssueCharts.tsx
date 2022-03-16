854416f78d4080ae8ec630ed915f1635d21a48d6
Ryan O'Shea <ryan.oshea@exitroute.org>
2022-03-16 23:45:29 +0100

    add user priority and status charts render based on select prop

--- app/issues/components/IssueCharts.tsx
+++ app/issues/components/IssueCharts.tsx
@@ -1,51 +1,123 @@
-import React from "react"
+import React, { Suspense } from "react"
 import { Box } from "@chakra-ui/react"
+import { useQuery } from "blitz"
+import getIssues from "../queries/getIssues"
 
 import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
   BarElement,
+  ArcElement,
   Title,
   Tooltip,
   Legend,
 } from "chart.js"
-import { Bar } from "react-chartjs-2"
+import { Bar, Doughnut } from "react-chartjs-2"
 import faker from "faker"
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+ChartJS.register(CategoryScale, LinearScale, ArcElement, BarElement, Title, Tooltip, Legend)
 
-export const options = {
-  maintainAspectRatio: false,
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top" as const,
+export function StatusChart({ selectedChart }) {
+  const [issues] = useQuery(getIssues, undefined)
+
+  const options = {
+    maintainAspectRatio: false,
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: false,
+        text: "Chart.js Bar Chart",
+      },
     },
-    title: {
-      display: false,
-      text: "Chart.js Bar Chart",
-    },
-  },
-}
+  }
 
-const labels = ["New", "In progress", "Closed"]
+  const statusLabels = ["New", "In progress", "Closed"]
 
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "Issues",
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 20 })),
-      backgroundColor: "#3182ce",
-    },
-  ],
-}
+  const statusData = {
+    labels: statusLabels,
+    datasets: [
+      {
+        label: "Progress Status",
+        data: statusLabels.map(() => faker.datatype.number({ min: 0, max: 20 })),
+        // backgroundColor: "#3182ce",
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          // "rgba(75, 192, 192, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          // "rgba(75, 192, 192, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  }
 
-export function StatusChart() {
+  const priorityLabels = ["Low", "Normal", "High"]
+
+  const priorityData = {
+    labels: priorityLabels,
+    datasets: [
+      {
+        label: "Priority",
+        data: priorityLabels.map(() => faker.datatype.number({ min: 0, max: 20 })),
+        // backgroundColor: "#3182ce",
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          // "rgba(75, 192, 192, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          // "rgba(75, 192, 192, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  }
+
+  const userLabels = ["Ryan", "James", "Felton", "Astrid"]
+
+  const userData = {
+    labels: userLabels,
+    datasets: [
+      {
+        label: "Users",
+        data: userLabels.map(() => faker.datatype.number({ min: 0, max: 20 })),
+        // backgroundColor: "#3182ce",
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  }
+
   return (
-    <Box paddingX="1rem" h="30vh" w="100%" position="relative">
-      <Bar options={options} data={data} />
+    <Box paddingX="1rem" h="50%" w="100%" position="relative">
+      {selectedChart === "status" && <Doughnut options={options} data={statusData} />}
+      {selectedChart === "priority" && <Doughnut options={options} data={priorityData} />}
+      {selectedChart === "users" && <Doughnut options={options} data={userData} />}
     </Box>
   )
 }
