7e83c8f37189f71a5351eead7cf1409742249ff1
Ryan O'Shea <ryan.oshea@exitroute.org>
2022-03-16 16:08:34 +0100

    add top border and drop shadow to bottom nav

--- app/core/components/Navigation.tsx
+++ app/core/components/Navigation.tsx
@@ -12,6 +12,10 @@ const Navigation = () => {
       align="center"
       h="4rem"
       display={{ base: "flex", md: "none" }}
+      borderTop="solid 1px"
+      borderColor="gray.200"
+      boxShadow="0 -4px 6px -1px rgb(0 0 0 / 10%), 0 -2px 4px -1px rgb(0 0 0 / 6%)"
+      zIndex="1"
     >
       {routes.map((route, index) => (
         <Link key={index} href={`/${route.toLowerCase()}`}>
