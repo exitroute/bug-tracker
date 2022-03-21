7d2f58a4898009ce8e193063dc9773b9ac112fb1
Ryan O'Shea <ryan.oshea@exitroute.org>
2022-03-21 22:41:22 +0100

    update getProjects query

--- app/projects/queries/getProjects.ts
+++ app/projects/queries/getProjects.ts
@@ -2,7 +2,31 @@ import db from "db"
 
 export default async function getProjects() {
   try {
-    const projects = await db.project.findMany()
+    const projects = await db.project.findMany({
+      include: {
+        assignedTeam: {
+          select: {
+            title: true,
+          },
+        },
+        assignedTo: {
+          select: {
+            name: true,
+          },
+        },
+        assignedIssues: {
+          select: {
+            priority: true,
+            status: true,
+          },
+        },
+        _count: {
+          select: {
+            assignedIssues: true,
+          },
+        },
+      },
+    })
     return projects
   } catch (error) {
     console.error("getIssues Error ", error)
