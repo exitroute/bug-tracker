e583761bf7641ce85a2bd7cb12bbdce24902a722
Ryan O'Shea <ryan.oshea@exitroute.org>
2022-03-22 18:12:22 +0100

    make user role selector available to admin only

--- app/users/components/UserProfileForm.tsx
+++ app/users/components/UserProfileForm.tsx
@@ -1,9 +1,9 @@
 import { Suspense } from "react"
-import { useQuery } from "blitz"
+import { useQuery, useSession } from "blitz"
 
-import { InputControl, TextareaControl, SelectControl } from "app/core/components/FormComponents"
+import { InputControl, SelectControl } from "app/core/components/FormComponents"
 
-import { Box, Stack, useColorModeValue } from "@chakra-ui/react"
+import { Box, Stack } from "@chakra-ui/react"
 
 import { Form } from "../../core/components/AppForm"
 export { FORM_ERROR } from "../../core/components/AppForm"
@@ -12,6 +12,7 @@ import getTeams from "app/teams/queries/getTeams"
 
 export const UserProfileForm = (props) => {
   const [teams] = useQuery(getTeams, undefined, { suspense: false })
+  const session = useSession()
 
   return (
     <Suspense fallback="Loading...">
@@ -21,6 +22,7 @@ export const UserProfileForm = (props) => {
             <Stack spacing={8}>
               <InputControl name="userProfile.name" placeholder="" label="Name" />
               <InputControl name="userProfile.email" placeholder="" label="Email" />
+              {/* TODO Add About */}
               {/* <TextareaControl name="userProfile.about" placeholder="" label="About" /> */}
               <SelectControl
                 name="userProfile.inTeams.id"
@@ -33,14 +35,16 @@ export const UserProfileForm = (props) => {
                   </option>
                 ))}
               </SelectControl>
-              <SelectControl name="userProfile.role" label="Role" placeholder="User role">
-                <option key={1} value="USER">
-                  USER
-                </option>
-                <option key={2} value="ADMIN">
-                  ADMIN
-                </option>
-              </SelectControl>
+              {session.role === "ADMIN" ? (
+                <SelectControl name="userProfile.role" label="Role" placeholder="User role">
+                  <option key={1} value="USER">
+                    USER
+                  </option>
+                  <option key={2} value="ADMIN">
+                    ADMIN
+                  </option>
+                </SelectControl>
+              ) : null}
             </Stack>
           </Form>
         </Stack>
