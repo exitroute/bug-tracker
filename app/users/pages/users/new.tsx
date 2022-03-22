3200ba9fc7f9e698d808ca67cfbd6ec2820d3be3
Ryan O'Shea <ryan.oshea@exitroute.org>
2022-03-22 16:37:09 +0100

    limit page authorisation

--- app/users/pages/users/new.tsx
+++ app/users/pages/users/new.tsx
@@ -1,6 +1,6 @@
-import { BlitzPage, Router, useMutation } from "blitz"
+import { BlitzPage, Router, useMutation, useSession } from "blitz"
 
-import { Flex, Stack, Heading, Text, useColorModeValue } from "@chakra-ui/react"
+import { Flex, Stack, Heading, Text, Center, useColorModeValue } from "@chakra-ui/react"
 
 import Layout from "app/core/layouts/Layout"
 import { UserProfileForm } from "app/users/components/UserProfileForm"
@@ -8,31 +8,40 @@ import createUser from "app/users/mutations/createUser"
 
 const NewUserPage: BlitzPage = () => {
   const initialValues = { userProfile: { name: "", about: "" } }
+  const session = useSession()
 
   const [createUserMutation] = useMutation(createUser)
 
   return (
     <Flex minH={"100vh"}>
-      <Stack spacing={8} mx={"auto"} w="100%" py={12} px={6}>
-        <Stack align={"center"}>
-          <Heading>New User</Heading>
-          <Text fontSize={"lg"} color={"gray.600"}>
-            Who is joining the team?
-          </Text>
+      {session.role === "ADMIN" ? (
+        <Stack spacing={8} mx={"auto"} w="100%" py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading>New User</Heading>
+            <Text fontSize={"lg"} color={"gray.600"}>
+              Who is joining the team?
+            </Text>
+          </Stack>
+          <UserProfileForm
+            onSubmit={async (values) => {
+              try {
+                const user = await createUserMutation(values)
+                Router.push(`/users/${user?.id}`)
+              } catch (error) {
+                console.error("CREATE USERPROFILE MUTATION ERROR", error)
+              }
+            }}
+            initialValues={initialValues}
+            submitText="Create User"
+          />
         </Stack>
-        <UserProfileForm
-          onSubmit={async (values) => {
-            try {
-              const user = await createUserMutation(values)
-              Router.push(`/users/${user?.id}`)
-            } catch (error) {
-              console.error("CREATE USERPROFILE MUTATION ERROR", error)
-            }
-          }}
-          initialValues={initialValues}
-          submitText="Create User"
-        />
-      </Stack>
+      ) : (
+        <Center w="100%">
+          <Heading as="h3" size="md" color="gray.500" textAlign="center">
+            You do not have access to this page.
+          </Heading>
+        </Center>
+      )}
     </Flex>
   )
 }
